ddd6b976ac77db04888fc141a093c34d77c9f4d9
Fabio Alcocer Sejas <fabio.alcocer@adtechnacity.com>
2024-02-26 21:31:36 -0400

    fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
-import './globals.css'
-import { cn } from '@/lib/utils'
-import { ThemeProvider } from '../components/theme-provider'
 import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Templo App',
@@ -20,13 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
-      >
+    <html lang='en'>
+      <body className={inter.className}>
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
