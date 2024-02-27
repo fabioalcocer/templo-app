f42fc082d10de7c2f34a90e230f3d70a6ea5d332
fabioalcocer <fabioalcocerdev@gmail.com>
2024-02-26 23:06:54 -0400

    add font mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import { ModeToggle } from '@/components/mode-toggle'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Templo App</h1>
+      <h1 className="font-mono text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        Templo App <br className="hidden sm:inline" />
+        built with Radix UI and Tailwind CSS.
+      </h1>
       <ModeToggle />
       <AddButton />
     </main>
