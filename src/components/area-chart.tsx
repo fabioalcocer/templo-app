a33b3975b8d2cca59f0d8b90ec45d2045622a26e
fabioalcocer <fabioalcocerdev@gmail.com>
2024-07-31 00:19:44 -0400

    add real data to area chart

--- src/components/area-chart.tsx
+++ src/components/area-chart.tsx
@@ -25,139 +25,135 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { getAllPayments, getAllPurchases, getSales } from '@/api'
 
-const chartData = [
-  { date: '2024-04-01', desktop: 222, mobile: 150 },
-  { date: '2024-04-02', desktop: 97, mobile: 180 },
-  { date: '2024-04-03', desktop: 167, mobile: 120 },
-  { date: '2024-04-04', desktop: 242, mobile: 260 },
-  { date: '2024-04-05', desktop: 373, mobile: 290 },
-  { date: '2024-04-06', desktop: 301, mobile: 340 },
-  { date: '2024-04-07', desktop: 245, mobile: 180 },
-  { date: '2024-04-08', desktop: 409, mobile: 320 },
-  { date: '2024-04-09', desktop: 59, mobile: 110 },
-  { date: '2024-04-10', desktop: 261, mobile: 190 },
-  { date: '2024-04-11', desktop: 327, mobile: 350 },
-  { date: '2024-04-12', desktop: 292, mobile: 210 },
-  { date: '2024-04-13', desktop: 342, mobile: 380 },
-  { date: '2024-04-14', desktop: 137, mobile: 220 },
-  { date: '2024-04-15', desktop: 120, mobile: 170 },
-  { date: '2024-04-16', desktop: 138, mobile: 190 },
-  { date: '2024-04-17', desktop: 446, mobile: 360 },
-  { date: '2024-04-18', desktop: 364, mobile: 410 },
-  { date: '2024-04-19', desktop: 243, mobile: 180 },
-  { date: '2024-04-20', desktop: 89, mobile: 150 },
-  { date: '2024-04-21', desktop: 137, mobile: 200 },
-  { date: '2024-04-22', desktop: 224, mobile: 170 },
-  { date: '2024-04-23', desktop: 138, mobile: 230 },
-  { date: '2024-04-24', desktop: 387, mobile: 290 },
-  { date: '2024-04-25', desktop: 215, mobile: 250 },
-  { date: '2024-04-26', desktop: 75, mobile: 130 },
-  { date: '2024-04-27', desktop: 383, mobile: 420 },
-  { date: '2024-04-28', desktop: 122, mobile: 180 },
-  { date: '2024-04-29', desktop: 315, mobile: 240 },
-  { date: '2024-04-30', desktop: 454, mobile: 380 },
-  { date: '2024-05-01', desktop: 165, mobile: 220 },
-  { date: '2024-05-02', desktop: 293, mobile: 310 },
-  { date: '2024-05-03', desktop: 247, mobile: 190 },
-  { date: '2024-05-04', desktop: 385, mobile: 420 },
-  { date: '2024-05-05', desktop: 481, mobile: 390 },
-  { date: '2024-05-06', desktop: 498, mobile: 520 },
-  { date: '2024-05-07', desktop: 388, mobile: 300 },
-  { date: '2024-05-08', desktop: 149, mobile: 210 },
-  { date: '2024-05-09', desktop: 227, mobile: 180 },
-  { date: '2024-05-10', desktop: 293, mobile: 330 },
-  { date: '2024-05-11', desktop: 335, mobile: 270 },
-  { date: '2024-05-12', desktop: 197, mobile: 240 },
-  { date: '2024-05-13', desktop: 197, mobile: 160 },
-  { date: '2024-05-14', desktop: 448, mobile: 490 },
-  { date: '2024-05-15', desktop: 473, mobile: 380 },
-  { date: '2024-05-16', desktop: 338, mobile: 400 },
-  { date: '2024-05-17', desktop: 499, mobile: 420 },
-  { date: '2024-05-18', desktop: 315, mobile: 350 },
-  { date: '2024-05-19', desktop: 235, mobile: 180 },
-  { date: '2024-05-20', desktop: 177, mobile: 230 },
-  { date: '2024-05-21', desktop: 82, mobile: 140 },
-  { date: '2024-05-22', desktop: 81, mobile: 120 },
-  { date: '2024-05-23', desktop: 252, mobile: 290 },
-  { date: '2024-05-24', desktop: 294, mobile: 220 },
-  { date: '2024-05-25', desktop: 201, mobile: 250 },
-  { date: '2024-05-26', desktop: 213, mobile: 170 },
-  { date: '2024-05-27', desktop: 420, mobile: 460 },
-  { date: '2024-05-28', desktop: 233, mobile: 190 },
-  { date: '2024-05-29', desktop: 78, mobile: 130 },
-  { date: '2024-05-30', desktop: 340, mobile: 280 },
-  { date: '2024-05-31', desktop: 178, mobile: 230 },
-  { date: '2024-06-01', desktop: 178, mobile: 200 },
-  { date: '2024-06-02', desktop: 470, mobile: 410 },
-  { date: '2024-06-03', desktop: 103, mobile: 160 },
-  { date: '2024-06-04', desktop: 439, mobile: 380 },
-  { date: '2024-06-05', desktop: 88, mobile: 140 },
-  { date: '2024-06-06', desktop: 294, mobile: 250 },
-  { date: '2024-06-07', desktop: 323, mobile: 370 },
-  { date: '2024-06-08', desktop: 385, mobile: 320 },
-  { date: '2024-06-09', desktop: 438, mobile: 480 },
-  { date: '2024-06-10', desktop: 155, mobile: 200 },
-  { date: '2024-06-11', desktop: 92, mobile: 150 },
-  { date: '2024-06-12', desktop: 492, mobile: 420 },
-  { date: '2024-06-13', desktop: 81, mobile: 130 },
-  { date: '2024-06-14', desktop: 426, mobile: 380 },
-  { date: '2024-06-15', desktop: 307, mobile: 350 },
-  { date: '2024-06-16', desktop: 371, mobile: 310 },
-  { date: '2024-06-17', desktop: 475, mobile: 520 },
-  { date: '2024-06-18', desktop: 107, mobile: 170 },
-  { date: '2024-06-19', desktop: 341, mobile: 290 },
-  { date: '2024-06-20', desktop: 408, mobile: 450 },
-  { date: '2024-06-21', desktop: 169, mobile: 210 },
-  { date: '2024-06-22', desktop: 317, mobile: 270 },
-  { date: '2024-06-23', desktop: 480, mobile: 530 },
-  { date: '2024-06-24', desktop: 132, mobile: 180 },
-  { date: '2024-06-25', desktop: 141, mobile: 190 },
-  { date: '2024-06-26', desktop: 434, mobile: 380 },
-  { date: '2024-06-27', desktop: 448, mobile: 490 },
-  { date: '2024-06-28', desktop: 149, mobile: 200 },
-  { date: '2024-06-29', desktop: 103, mobile: 160 },
-  { date: '2024-06-30', desktop: 446, mobile: 400 },
-]
+type NumberKey = keyof typeof filtersDateObj
+
+interface ChartData {
+  date: string
+  expenses: number
+  revenues: number
+}
+
+// DAY_MS is the number of milliseconds in a day
+const DAY_MS = 86400000
+
+const filtersDateObj = {
+  '180d': 180,
+  '90d': 90,
+  '30d': 30,
+  '7d': 7,
+}
 
 const chartConfig = {
-  visitors: {
-    label: 'Visitors',
-  },
-  desktop: {
-    label: 'Desktop',
+  revenues: {
+    label: 'Ingresos',
     color: 'hsl(var(--chart-1))',
   },
-  mobile: {
-    label: 'Mobile',
+  expenses: {
+    label: 'Gastos',
     color: 'hsl(var(--chart-2))',
   },
 } satisfies ChartConfig
 
+const parsedDate = (date: number) => {
+  const parseDate = new Date(date)
+  return parseDate.toISOString()
+}
+
 export function AreaChartComponent() {
+  const [chartData, setChartData] = React.useState<ChartData[]>([])
   const [timeRange, setTimeRange] = React.useState('90d')
 
-  const filteredData = chartData.filter((item) => {
+  const filteredData = chartData?.filter((item) => {
     const date = new Date(item.date)
     const now = new Date()
     let daysToSubtract = 90
-    if (timeRange === '30d') {
-      daysToSubtract = 30
-    } else if (timeRange === '7d') {
-      daysToSubtract = 7
+
+    if (timeRange !== '90d') {
+      daysToSubtract = filtersDateObj[timeRange as NumberKey]
     }
+
     now.setDate(now.getDate() - daysToSubtract)
     return date >= now
   })
+
+  const parsedData = async ({
+    sales,
+    purchases,
+    payments,
+  }: {
+    sales: Sale[]
+    purchases: Purchase[]
+    payments: Payment[]
+  }) => {
+    const startDate = Math.min(
+      ...sales.map((sale) => new Date(sale.createdAt).getTime()),
+      ...purchases.map((purchase) => new Date(purchase.createdAt).getTime()),
+      ...payments.map((payment) => new Date(payment.createdAt).getTime()),
+    )
+    const endDate = Math.max(
+      ...sales.map((sale) => new Date(sale.createdAt).getTime()),
+      ...purchases.map((purchase) => new Date(purchase.createdAt).getTime()),
+      ...payments.map((payment) => new Date(payment.createdAt).getTime()),
+    )
+
+    const dates = []
+    for (let date = startDate; date <= endDate; date += DAY_MS) {
+      dates.push(new Date(date + DAY_MS))
+    }
+
+    const result = dates.map((date) => {
+      const offset = date.getTimezoneOffset()
+      const adjustedDate = new Date(date.getTime() + offset * 60 * 1000)
+      const dateString = adjustedDate.toISOString().split('T')[0]
+
+      const revenues = sales
+        .filter((sale) => parsedDate(sale.createdAt).startsWith(dateString))
+        .reduce((acc, sale) => acc + sale.total, 0)
+      const paymentsRevenues = payments
+        .filter((payment) =>
+          parsedDate(payment.createdAt).startsWith(dateString),
+        )
+        .reduce((acc, payment) => acc + payment.finalPrice, 0)
+      const totalRevenues = revenues + paymentsRevenues
+
+      const expenses = purchases
+        .filter((purchase) =>
+          parsedDate(purchase.createdAt).startsWith(dateString),
+        )
+        .reduce((acc, purchase) => acc + purchase.cost * purchase?.stock, 0)
+
+      return {
+        date: dateString,
+        revenues: totalRevenues,
+        expenses,
+      }
+    })
+
+    setChartData(result)
+  }
+
+  const fetchData = async () => {
+    const sales = await getSales()
+    const purchases = await getAllPurchases()
+    const payments = await getAllPayments()
+
+    await parsedData({ sales, purchases, payments })
+  }
+
+  React.useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <Card>
       <CardHeader className='flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row'>
         <div className='grid flex-1 gap-1 text-center sm:text-left'>
-          <CardTitle>Gráfico de área - Usuarios Activos</CardTitle>
+          <CardTitle>Ingresos y Gastos</CardTitle>
           <CardDescription>
-            Mostrando el número de usuarios activos en los últimos{' '}
-            {timeRange === '90d' ? '90' : '30'} días
+            Mostrando el total de ingresos y gastos en los últimos{' '}
+            {filtersDateObj[timeRange as NumberKey]} días
           </CardDescription>
         </div>
         <Select value={timeRange} onValueChange={setTimeRange}>
@@ -168,14 +164,17 @@ export function AreaChartComponent() {
             <SelectValue placeholder='Last 3 months' />
           </SelectTrigger>
           <SelectContent className='rounded-xl'>
+            <SelectItem value='180d' className='rounded-lg'>
+              Últimos 6 meses
+            </SelectItem>
             <SelectItem value='90d' className='rounded-lg'>
-              Last 3 months
+              Últimos 3 meses
             </SelectItem>
             <SelectItem value='30d' className='rounded-lg'>
-              Last 30 days
+              Últimos 30 días
             </SelectItem>
             <SelectItem value='7d' className='rounded-lg'>
-              Last 7 days
+              Últimos 7 días
             </SelectItem>
           </SelectContent>
         </Select>
@@ -190,24 +189,24 @@ export function AreaChartComponent() {
               <linearGradient id='fillDesktop' x1='0' y1='0' x2='0' y2='1'>
                 <stop
                   offset='5%'
-                  stopColor='var(--color-desktop)'
+                  stopColor='var(--color-expenses)'
                   stopOpacity={0.8}
                 />
                 <stop
                   offset='95%'
-                  stopColor='var(--color-desktop)'
+                  stopColor='var(--color-expenses)'
                   stopOpacity={0.1}
                 />
               </linearGradient>
               <linearGradient id='fillMobile' x1='0' y1='0' x2='0' y2='1'>
                 <stop
                   offset='5%'
-                  stopColor='var(--color-mobile)'
+                  stopColor='var(--color-revenues)'
                   stopOpacity={0.8}
                 />
                 <stop
                   offset='95%'
-                  stopColor='var(--color-mobile)'
+                  stopColor='var(--color-revenues)'
                   stopOpacity={0.1}
                 />
               </linearGradient>
@@ -237,22 +236,22 @@ export function AreaChartComponent() {
                       day: 'numeric',
                     })
                   }}
-                  indicator='dot'
+                  indicator='line'
                 />
               }
             />
             <Area
-              dataKey='mobile'
+              dataKey='revenues'
               type='natural'
               fill='url(#fillMobile)'
-              stroke='var(--color-mobile)'
+              stroke='var(--color-revenues)'
               stackId='a'
             />
             <Area
-              dataKey='desktop'
+              dataKey='expenses'
               type='natural'
               fill='url(#fillDesktop)'
-              stroke='var(--color-desktop)'
+              stroke='var(--color-expenses)'
               stackId='a'
             />
             <ChartLegend content={<ChartLegendContent />} />
