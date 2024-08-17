81e3c30a8415eba7007de5333c32ee2c49debd27
fabioalcocer <fabioalcocerdev@gmail.com>
2024-08-17 00:33:09 -0400

    add pie chart to show users by discipline

--- src/app/admin/analytics/page.tsx
+++ src/app/admin/analytics/page.tsx
@@ -2,40 +2,45 @@
 import { AreaChartComponent } from '@/components/area-chart'
 import { BarsChartComponent } from '@/components/bar-chart'
 import { LineChartComponent } from '@/components/line-chart'
+import { PieChartComponent } from '@/components/pie-chart'
 import { LineChart } from 'lucide-react'
 import { useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 
 export default function SalesPage() {
-  const session = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/login')
-    },
-  })
+	const session = useSession({
+		required: true,
+		onUnauthenticated() {
+			redirect('/login')
+		},
+	})
 
-  if (!session) {
-    return null
-  }
+	if (!session) {
+		return null
+	}
 
-  return (
-    <div className='flex flex-col gap-4'>
-      <h2 className='mb-5 flex items-center justify-center gap-3 text-center text-3xl font-semibold'>
-        <LineChart width={36} height={36} />
-        Analíticas
-      </h2>
+	return (
+		<div className="flex flex-col gap-4">
+			<h2 className="mb-5 flex items-center justify-center gap-3 text-center text-3xl font-semibold">
+				<LineChart width={36} height={36} />
+				Analíticas
+			</h2>
 
-      <AreaChartComponent />
-      <section className='mt-3 flex flex-wrap gap-5 md:gap-4'>
-        <div className='w-full max-w-none md:max-w-sm'>
-          <BarsChartComponent />
-        </div>
-        <div className='w-full max-w-none md:max-w-sm'>
-          <LineChartComponent />
-        </div>
-      </section>
-    </div>
-  )
+			<AreaChartComponent />
+			<section className="mt-3 grid grid-cols-3 gap-5 md:gap-4">
+				<div className="w-full max-w-none md:max-w-sm">
+					<PieChartComponent />
+				</div>
+
+				<div className="w-full max-w-none md:max-w-sm">
+					<LineChartComponent />
+				</div>
+				<div className="w-full max-w-none md:max-w-sm">
+					<BarsChartComponent />
+				</div>
+			</section>
+		</div>
+	)
 }
 
 SalesPage.requireAuth = true
---
+++ src/components/pie-chart.tsx
@@ -0,0 +1,158 @@
+'use client'
+
+import { TrendingUp } from 'lucide-react'
+import * as React from 'react'
+import { Label, Pie, PieChart } from 'recharts'
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+
+import { getAllUsers } from '@/api'
+import {
+	type ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from '@/components/ui/chart'
+
+const chartConfig = {
+	calistenia: {
+		label: 'Calistenia',
+		color: ' hsl(var(--primary))',
+	},
+	'power-plate': {
+		label: 'Power Plate',
+		color: '#e11d48',
+	},
+	custom: {
+		label: 'Personalizado',
+		color: '#2563eb',
+	},
+	other: {
+		label: 'Otras',
+		color: 'hsl(var(--chart-4))',
+	},
+} satisfies ChartConfig
+
+type ChartData = {
+	discipline: string
+	users: number
+	fill: string
+}
+
+export function PieChartComponent() {
+	const [chartData, setChartData] = React.useState<ChartData[]>([])
+
+	function getDisciplineChartData(users: User[]) {
+		const disciplineCounts: Record<string, number> = {}
+
+		for (const user of users) {
+			const discipline = user.discipline || 'other'
+			disciplineCounts[discipline] = (disciplineCounts[discipline] || 0) + 1
+		}
+
+		const colors = {
+			calistenia: 'var(--color-calistenia)',
+			'power-plate': 'var(--color-power-plate)',
+			custom: 'var(--color-custom)',
+			other: 'var(--color-other)',
+		}
+
+		const chartData = Object.entries(disciplineCounts).map(
+			([discipline, count]) => ({
+				discipline,
+				users: count,
+				fill: colors[discipline as keyof typeof colors] || colors.other,
+			}),
+		)
+
+		return chartData
+	}
+
+	const totalVisitors = React.useMemo(() => {
+		return chartData?.reduce((acc, curr) => acc + curr.users, 0)
+	}, [chartData])
+
+	const fetchData = async () => {
+		const users = await getAllUsers()
+		const chartData = getDisciplineChartData(users)
+		setChartData(chartData)
+	}
+
+	React.useEffect(() => {
+		fetchData()
+	}, [])
+
+	return (
+		<Card className="flex flex-col">
+			<CardHeader className="items-center pb-0">
+				<CardTitle>Usuarios - Disciplinas</CardTitle>
+				<CardDescription>Enero - Agosto 2024</CardDescription>
+			</CardHeader>
+			<CardContent className="flex-1 pb-0">
+				<ChartContainer
+					config={chartConfig}
+					className="mx-auto aspect-square max-h-[250px]"
+				>
+					<PieChart>
+						<ChartTooltip
+							cursor={false}
+							content={<ChartTooltipContent hideLabel />}
+						/>
+						<Pie
+							data={chartData}
+							dataKey="users"
+							nameKey="discipline"
+							innerRadius={60}
+							strokeWidth={5}
+						>
+							<Label
+								content={({ viewBox }) => {
+									if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+										return (
+											<text
+												x={viewBox.cx}
+												y={viewBox.cy}
+												textAnchor="middle"
+												dominantBaseline="middle"
+											>
+												<tspan
+													x={viewBox.cx}
+													y={viewBox.cy}
+													className="fill-foreground text-3xl font-bold"
+												>
+													{totalVisitors.toLocaleString()}
+												</tspan>
+												<tspan
+													x={viewBox.cx}
+													y={(viewBox.cy || 0) + 24}
+													className="fill-muted-foreground"
+												>
+													Usuarios
+												</tspan>
+											</text>
+										)
+									}
+								}}
+							/>
+						</Pie>
+					</PieChart>
+				</ChartContainer>
+			</CardContent>
+			<CardFooter className="flex-col gap-2 text-sm">
+				<div className="flex items-center gap-2 font-medium leading-none">
+					Creció un 2.2% en este mes <TrendingUp className="h-4 w-4" />
+				</div>
+				<div className="leading-none text-muted-foreground">
+					Total de usuarios en los últimos 6 meses
+				</div>
+			</CardFooter>
+		</Card>
+	)
+}
