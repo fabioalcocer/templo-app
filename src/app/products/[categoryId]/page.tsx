a73dbbb077ea40539e0a9a1c4d242758a6bb46ce
fabioalcocer <fabioalcocerdev@gmail.com>
2024-10-07 01:16:37 -0400

    add supabase

--- src/app/products/[categoryId]/page.tsx
+++ src/app/products/[categoryId]/page.tsx
@@ -1,23 +1,27 @@
 import { getProductsByCategoryId } from '@/api'
 import ProductsList from '@/components/products-list'
+import { getProducts } from '@/utils/supabase/queries'
 import { Suspense } from 'react'
 import Loading from './loading'
 
 export const revalidate = 3600
 
 type Props = {
-  params: {
-    categoryId: string
-  }
+	params: {
+		categoryId: string
+	}
 }
 
 async function ProductsCategoryPage({ params: { categoryId } }: Props) {
-  const products = await getProductsByCategoryId(categoryId)
-  return (
-    <Suspense fallback={<Loading />}>
-      <ProductsList products={products} />
-    </Suspense>
-  )
+	const productsSupabase = await getProducts()
+	console.log(productsSupabase)
+	const products = await getProductsByCategoryId(categoryId)
+
+	return (
+		<Suspense fallback={<Loading />}>
+			<ProductsList products={products} />
+		</Suspense>
+	)
 }
 
 export default ProductsCategoryPage
