686cd73de8573bdd12c494172e380b7cf8d0bccc
fabioalcocer <fabioalcocerdev@gmail.com>
2025-01-22 19:13:00 -0400

    fix header logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,26 @@
 'use client'
 import Logo from '@/assets/logo-black.png'
-import LogoWhite from '@/assets/logo-white.png'
 
 import { UserButton } from '@stackframe/stack'
 import { LayoutDashboard, StoreIcon } from 'lucide-react'
-import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
 import { ModeToggle } from './mode-toggle'
 
 function Header() {
-	const [isDarkTheme, setIsDarkTheme] = useState(false)
-	const { resolvedTheme } = useTheme()
 	const router = useRouter()
-
-	useEffect(() => {
-		setIsDarkTheme(resolvedTheme === 'dark')
-	}, [resolvedTheme])
 
 	return (
 		<header className="sticky top-0 z-50 flex w-full bg-background px-0 py-3 shadow-sm rounded-lg md:p-5 md:py-4">
 			<div className="mx-auto flex w-full max-w-[1450px] items-center justify-between px-3">
 				<Link href="/" className="flex items-center gap-3">
 					<Image
-						src={isDarkTheme ? LogoWhite : Logo}
+						src={Logo}
 						width={150}
-						alt="Templo logo blanco"
-						className="mr-3"
+						alt="Logo templo"
+						className="mr-3 dark:invert"
 					/>
-					{/* <h1 className='font-mono text-2xl font-semibold uppercase'>Templo</h1> */}
 				</Link>
 
 				<div className="flex items-center gap-3">
